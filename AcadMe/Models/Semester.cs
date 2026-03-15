07d5a158af5083a1fde4d51fad9b5bd02977c2e0
unknown <adkhilal@gmail.com>
2026-03-16 00:16:03 +0530

    Add semester model

---
+++ AcadMe/Models/Semester.cs
@@ -0,0 +1,11 @@
+﻿namespace AcadMe.Models
+{
+    public class Semester
+    {
+        public int Id { get; set; }
+        public int SemNo { get; set; }
+        public string AcademicYear { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public bool ResultsIn { get; set; }
+    }
+}
