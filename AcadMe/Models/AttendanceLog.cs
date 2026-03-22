913ff69e2960642e8343e9b20ee213b09813711d
unknown <adkhilal@gmail.com>
2026-03-22 23:24:41 +0530

    Added attendancelog model

---
+++ AcadMe/Models/AttendanceLog.cs
@@ -0,0 +1,13 @@
+﻿namespace AcadMe.Models
+{
+    public class AttendanceLog
+    {
+        public int Id { get; set; }
+        public int SubjectId { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public string Note { get; set; } = string.Empty;
+
+        public Subject Subject { get; set; } = null!;
+    }
+}
