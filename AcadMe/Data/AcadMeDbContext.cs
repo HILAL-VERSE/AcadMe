91c61b56651ab580e411c9eb08768615784d944a
unknown <adkhilal@gmail.com>
2026-03-28 19:46:08 +0530

    Add dbcontext with six dbsets

---
+++ AcadMe/Data/AcadMeDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AcadMe.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcadMe.Data
+{
+    public class AcadMeDbContext : DbContext
+    {
+        public DbSet<AttendanceLog> AttendanceLogs { get; set; }
+        public DbSet<PersonalTask> PersonalTasks { get; set; }
+        public DbSet<PracticalLog> PracticalLogs { get; set; }
+        public DbSet<Result> Results { get; set; }
+        public DbSet<Semester> Semesters { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source = acadme.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
