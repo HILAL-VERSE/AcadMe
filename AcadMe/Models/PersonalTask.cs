41180b6d57716d2b63d96b8d7c9066ec49718df2
unknown <adkhilal@gmail.com>
2026-03-28 17:47:54 +0530

    Add personal task modal

---
+++ AcadMe/Models/PersonalTask.cs
@@ -0,0 +1,12 @@
+﻿namespace AcadMe.Models
+{
+    public class PersonalTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? DeadLine { get; set; }
+        public string Priority { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+}
