b8a4a0434a1a50e906fbba7a92a5838f8fe82a78
unknown <adkhilal@gmail.com>
2026-04-02 16:03:19 +0530

    Add Relay Command and BaseViewModel

---
+++ AcadMe/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AcadMe.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
