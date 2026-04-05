dd1c740d53058ef7b4d7cd81b24688673604e448
unknown <adkhilal@gmail.com>
2026-04-05 07:28:26 +0530

    Add navigation shell with working sidebar and SubjectsView placeholder

--- AcadMe/MainWindow.xaml.cs
+++ AcadMe/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using AcadMe.Views;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,5 +21,41 @@ namespace AcadMe
         {
             InitializeComponent();
         }
+
+        public void Dashboard_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new TextBlock { Text = "Dashboard Screen Coming Soon", FontSize = 24 };
+        }
+
+        public void Subject_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new SubjectsView();
+        }
+
+        public void Attendance_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new TextBlock { Text = "Attendance Screen Coming Soon", FontSize = 24 };
+        }
+
+        public void Practicals_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new TextBlock { Text = "Practicals Screen Coming Soon", FontSize = 24 };
+        }
+
+        public void Tasks_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new TextBlock { Text = "Tasks Screen Coming Soon", FontSize = 24 };
+        }
+
+        public void Results_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new TextBlock { Text = "Result Screen Coming Soon", FontSize = 24 };
+        }
+
+        public void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new TextBlock { Text = "Settings Screen Coming Soon", FontSize = 24 };
+        }
     }
+
 }
